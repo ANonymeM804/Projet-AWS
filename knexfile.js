f27c28ffc767a068fb3fcff422f49ed4009eab62
zvezda123 <alyciabelkessam123@gmail.com>
2026-04-06 15:35:48 +0200

    modification configuration knex pour render

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
 module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: false  // local, pas besoin de SSL
-    }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './migrations'
+    },
+    ssl: { rejectUnauthorized: false }
   }
 };
